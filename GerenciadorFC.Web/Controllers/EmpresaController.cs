18f219bac3ee790cd5cfd24e58995ccebe075e0f
Erivelto <fabioesimoes@gmail.com>
2018-03-31 07:48:49 -0300

    corresções de texto e formlarios

---
+++ GerenciadorFC.Web/Controllers/EmpresaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorFC.Web.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult AbrirEmpresa()
+		{
+			return View();
+		}
+    }
+}
--- GerenciadorFC.Web/Controllers/HomeController.cs
+++ GerenciadorFC.Web/Controllers/HomeController.cs
@@ -67,5 +67,9 @@ namespace WebSite.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+		public IActionResult Contato()
+		{
+			return View();
+		}
     }
 }
